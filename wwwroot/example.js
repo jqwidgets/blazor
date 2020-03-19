73651231d9c5503877622a53fb75daf8de865e99
Ivo Zhulev <ivo.zhulev@gmail.com>
2020-03-19 20:39:43 +0200

    defaultfunctionality (#2)

--- wwwroot/example.js
+++ wwwroot/example.js
@@ -1,34 +1,5 @@
 window.Example = {
-    buildColumns: function() {
-        const columns =  [
-            {
-                text: 'First Name2', datafield: 'firstName', width: 100,
-                createwidget: "createwidget",
-                initwidget: function (row, column, value, htmlElement) {
-                    console.log(row, column, value, htmlElement);
-                }
-            },
-            { text: 'Last Name', datafield: 'lastName', width: 200 }
-        ];
-
-        return columns;
-    },
-
-    
-    initwidget: function(row, column, value, htmlElement) {
-        console.log(row, column, value, htmlElement);
-    },
-    test: function(source) {
-        return new jqx.dataadapter(source);
-    },
-
-    updateInnerHtml: function(a, b) {
-        console.log(a, b);
+    updateInnerHtml: function(element, checked) {
+        element.innerHTML = checked ? 'Checked': 'Unchecked';
     } 
 };
-
-function createwidget(row, column, value, htmlElement) {
-    console.log(row, column, value, htmlElement);
-}
-
-//JSRuntime.InvokeAsync<object>("Example.test", source);
