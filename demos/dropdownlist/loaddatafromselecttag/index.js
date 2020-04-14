76f11ce10363d631b42934b8c8c4a77f41b808bb
Hristo <hr100h@gmail.com>
2020-04-14 21:26:48 +0300

    Checkboxes, Auto Open, Load data from select tag demos
    Also, update on the Default Functionality demo

--- demos/dropdownlist/loaddatafromselecttag/index.js
+++ demos/dropdownlist/loaddatafromselecttag/index.js
@@ -1,5 +1,13 @@
 window.Example = {
     handleSelect: function (updating, item, element) {
         element.value = item.value;
+    },
+    handleChange: function (selectComponent, items, value) {
+        for (let i = 0; i < items.length; i++) {
+            const element = items[i];
+            if (element.value === value) {
+                return element.index;
+            }
+        }
     }
 };
