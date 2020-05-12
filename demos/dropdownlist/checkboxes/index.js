a59107396e976a4325ee1b3cdf3fbaaf12d01896
jQWidgets <info@jqwidgets.com>
2020-05-12 13:19:18 +0300

    Dropdownlist (#10)

--- demos/dropdownlist/checkboxes/index.js
+++ demos/dropdownlist/checkboxes/index.js
@@ -3,9 +3,8 @@ window.Example = {
         selectionlog.innerHTML = "<div>Label: " + item.label + "</div>" +
             "<div>Value: " + item.value + "</div>" +
             "<div>Checked: " + item.checked + "</div>";
-        var result = "";     
+        let result = "";     
         for (let i = 0; i < items.length; i++) {
-            const element = items[i];
             result += items[i].label + ", ";
         }
 
