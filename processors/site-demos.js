2b4d87ed35d4d1f0af7302aa4bd8420538ea5849
Ivo Zhulev <ivo.zhulev@gmail.com>
2020-08-28 11:15:45 +0300

    Update demos (#53)

--- processors/site-demos.js
+++ processors/site-demos.js
@@ -47,7 +47,7 @@ async function generateSiteDemos() {
                 }
 
                 // // uncomment to test a single demo
-                // if (!(widgetFolder === 'grid' && demoFolder === 'bindingtoarray')) {
+                // if (!(widgetFolder === 'grid' && demoFolder === 'bindingtojson')) {
                 //     continue;
                 // }
 
